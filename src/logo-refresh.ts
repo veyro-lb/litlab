3b6da74a2e9fa083173f2d58196736b3e70dc779
veyro-lb <veyro.202@gmail.com>
2026-08-28 13:00:11 +0300

    Integrate LitLab bulb base with i dot

--- src/logo-refresh.ts
+++ src/logo-refresh.ts
@@ -6,18 +6,18 @@ function createFullLogoDecorations(){
   decorations.className='litlab-logo-decorations';
   decorations.setAttribute('aria-hidden','true');
   decorations.innerHTML=`
-    <svg class="litlab-bulb-overlay" viewBox="0 0 44 40" aria-hidden="true">
-      <circle class="litlab-bulb-halo" cx="22" cy="16" r="14"/>
+    <svg class="litlab-bulb-overlay" viewBox="0 0 40 40" aria-hidden="true">
+      <circle class="litlab-bulb-halo" cx="20" cy="14" r="11.5"/>
       <g class="litlab-bulb-rays" fill="none" stroke-linecap="round">
-        <path d="M22 1.5v4"/>
-        <path d="M9.3 6.3l2.9 2.9"/>
-        <path d="M34.7 6.3l-2.9 2.9"/>
+        <path d="M20 1.8v3.2"/>
+        <path d="M9.8 5.8l2.2 2.2"/>
+        <path d="M30.2 5.8 28 8"/>
       </g>
-      <path class="litlab-bulb-glass" d="M14.5 15.8c0-4.6 3.4-8.3 7.5-8.3s7.5 3.7 7.5 8.3c0 3.2-1.5 5.1-3.9 7.3-1.1 1-1.7 2-1.8 3.2h-3.6c-.1-1.2-.7-2.2-1.8-3.2-2.4-2.2-3.9-4.1-3.9-7.3Z"/>
-      <path class="litlab-bulb-filament" d="M19.5 17.4 22 20l2.5-2.6M22 20v6.2"/>
+      <path class="litlab-bulb-glass" d="M13.8 14.5c0-3.9 2.8-7 6.2-7s6.2 3.1 6.2 7c0 2.7-1.2 4.3-3.2 6.2-.9.9-1.4 1.8-1.5 2.8h-3c-.1-1-.6-1.9-1.5-2.8-2-1.9-3.2-3.5-3.2-6.2Z"/>
+      <path class="litlab-bulb-filament" d="m17.9 15.7 2.1 2.1 2.1-2.1M20 17.8v5.6"/>
       <g class="litlab-bulb-base">
-        <path d="M18.9 29.1h6.2"/>
-        <circle cx="22" cy="33.2" r="3.1"/>
+        <path d="M17.8 26.1h4.4"/>
+        <circle cx="20" cy="31.5" r="2.55"/>
       </g>
     </svg>
     <span class="litlab-floating-star"></span>
