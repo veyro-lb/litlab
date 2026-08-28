4b446eace3b2f24bce8ee99ae70500b0db839b38
veyro-lb <veyro.202@gmail.com>
2026-08-28 13:07:37 +0300

    Make original i dot the LitLab bulb base

--- src/logo-refresh.ts
+++ src/logo-refresh.ts
@@ -6,18 +6,18 @@ function createFullLogoDecorations(){
   decorations.className='litlab-logo-decorations';
   decorations.setAttribute('aria-hidden','true');
   decorations.innerHTML=`
-    <svg class="litlab-bulb-overlay" viewBox="0 0 40 40" aria-hidden="true">
-      <circle class="litlab-bulb-halo" cx="20" cy="14" r="11.5"/>
+    <svg class="litlab-bulb-overlay" viewBox="0 0 40 32" aria-hidden="true">
+      <circle class="litlab-bulb-halo" cx="20" cy="12.5" r="10.5"/>
       <g class="litlab-bulb-rays" fill="none" stroke-linecap="round">
-        <path d="M20 1.8v3.2"/>
-        <path d="M9.8 5.8l2.2 2.2"/>
-        <path d="M30.2 5.8 28 8"/>
+        <path d="M20 1.5v3"/>
+        <path d="M9.8 5.6 12 7.8"/>
+        <path d="M30.2 5.6 28 7.8"/>
       </g>
-      <path class="litlab-bulb-glass" d="M13.8 14.5c0-3.9 2.8-7 6.2-7s6.2 3.1 6.2 7c0 2.7-1.2 4.3-3.2 6.2-.9.9-1.4 1.8-1.5 2.8h-3c-.1-1-.6-1.9-1.5-2.8-2-1.9-3.2-3.5-3.2-6.2Z"/>
-      <path class="litlab-bulb-filament" d="m17.9 15.7 2.1 2.1 2.1-2.1M20 17.8v5.6"/>
+      <path class="litlab-bulb-glass" d="M13.7 13c0-4 2.8-7.2 6.3-7.2s6.3 3.2 6.3 7.2c0 2.8-1.3 4.6-3.3 6.4-.9.8-1.4 1.7-1.5 2.8h-3c-.1-1.1-.6-2-1.5-2.8-2-1.8-3.3-3.6-3.3-6.4Z"/>
+      <path class="litlab-bulb-filament" d="m17.8 14.1 2.2 2.2 2.2-2.2M20 16.3v5.8"/>
       <g class="litlab-bulb-base">
-        <path d="M17.8 26.1h4.4"/>
-        <circle cx="20" cy="31.5" r="2.55"/>
+        <path d="M17.4 24.9h5.2"/>
+        <path d="M18.3 27.8h3.4"/>
       </g>
     </svg>
     <span class="litlab-floating-star"></span>
