023de7991cad6ab87515bbf77ff84849869c25e0
veyro-lb <veyro.202@gmail.com>
2026-08-28 12:52:55 +0300

    Refine LitLab bulb proportions and move star to Lab

--- src/logo-refresh.ts
+++ src/logo-refresh.ts
@@ -6,18 +6,20 @@ function createFullLogoDecorations(){
   decorations.className='litlab-logo-decorations';
   decorations.setAttribute('aria-hidden','true');
   decorations.innerHTML=`
-    <svg class="litlab-bulb-overlay" viewBox="0 0 64 64" aria-hidden="true">
-      <circle class="litlab-bulb-halo" cx="32" cy="28" r="23"/>
+    <svg class="litlab-bulb-overlay" viewBox="0 0 48 56" aria-hidden="true">
+      <circle class="litlab-bulb-halo" cx="24" cy="21" r="17"/>
       <g class="litlab-bulb-rays" fill="none" stroke-linecap="round">
-        <path d="M32 3v7"/>
-        <path d="M12.5 11.5l5 5"/>
-        <path d="M51.5 11.5l-5 5"/>
-        <path d="M6 29h7"/>
-        <path d="M51 29h7"/>
+        <path d="M24 1.8v5.4"/>
+        <path d="M8.6 7.8l3.8 3.8"/>
+        <path d="M39.4 7.8l-3.8 3.8"/>
       </g>
-      <path class="litlab-bulb-glass" d="M19 27.5c0-7.5 5.8-13.5 13-13.5s13 6 13 13.5c0 5.3-2.6 8.6-6 11.8-1.8 1.7-2.7 3.2-2.8 5.2h-8.4c-.1-2-.9-3.5-2.8-5.2-3.4-3.2-6-6.5-6-11.8Z"/>
-      <path class="litlab-bulb-filament" d="M27.5 29.5 32 34l4.5-4.5M32 34v10.5"/>
-      <path class="litlab-bulb-base" d="M27 49h10M28.5 53h7"/>
+      <path class="litlab-bulb-glass" d="M14.7 21.2c0-5.8 4.2-10.4 9.3-10.4s9.3 4.6 9.3 10.4c0 4-1.8 6.6-4.8 9.4-1.3 1.2-2 2.5-2.1 4H21.6c-.1-1.5-.8-2.8-2.1-4-3-2.8-4.8-5.4-4.8-9.4Z"/>
+      <path class="litlab-bulb-filament" d="M20.7 22.7 24 26l3.3-3.3M24 26v8.5"/>
+      <g class="litlab-bulb-base">
+        <path d="M19.8 38h8.4"/>
+        <path d="M20.8 41.5h6.4"/>
+        <circle cx="24" cy="45.3" r="2.7"/>
+      </g>
     </svg>
     <span class="litlab-floating-star"></span>
   `;
